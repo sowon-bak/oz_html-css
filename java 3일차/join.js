2fbe195e4e493f333167dde92265f48585fba6ff
sowon-bak <cruzean479@gmail.com>
2025-09-16 01:14:47 +0900

    회원가입

---
+++ java 3일차/join.js
@@ -0,0 +1,28 @@
+const form = document.getElementById('form')
+
+form.addEventListener('submit', function(event){
+    event.preventDefault()
+
+    let userid = event.target.id.value
+    let userpw1 = event.target.pw1.value
+    let userpw2 = event.target.pw2.value
+    let username = event.target.name.value
+    let userphone = event.target.phone.value
+    let userposition = event.target.position.value
+    let usergender = event.target.gender.value
+    let useremail = event.target.email.value
+    let userintro = event.target.intro.value
+    
+    if(userid.length < 6){
+        alert('아이디가 너무 짧습니다. 6자 이상 입력해주세요.')
+        return 
+    }
+
+    if(userpw1 !== userpw2){
+        alert('비밀번호가 일치하지 않습니다')
+        return
+    }
+
+    document.body.innerHTML = ''
+    document.write(`<p>${userid}님 환영합니다<p>`)
+})
